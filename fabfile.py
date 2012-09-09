cd0cda96c53622f021e43540bde5e342e75f3fa9
Vince Veselosky <vince@control-escape.com>
2012-09-09 05:15:53 -0400

    track latest otto

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,13 @@
-from fabric.api import env, local, task
-import otto.git as git
-import otto.web as web
+from fabric.api import env, lcd, local, task
+import otto.blog as blog
+from otto.util import paths
 
-env['otto.git.remote_repo_path'] = '/home/vince/Repo'
-env['otto.web.build_dir'] = './build/staged'
-env['otto.web.site'] = 'www.webquills.net'
+import logging
+logging.basicConfig(level=logging.INFO)
+
+env['otto.build_dir'] = 'build/staged'
+#env['otto.template_dir'] = './templates'
+env['otto.site'] = 'www.webquills.net'
 
 @task
 def B():
@@ -14,7 +17,16 @@ def B():
 @task
 def build():
     """Build the site"""
-    target = env['otto.web.build_dir']
-    local('rm -rf build/*') # make clean
-    local('cp -a etc %s/' % target)
+    target = env['otto.build_dir']
+    with lcd(paths.local_workspace()):
+        local('rm -rf build/*') # make clean
+        local('mkdir -p %s' % target)
+        local('cp -a etc %s/' % target)
+        # local('cp -a content %s/htdocs' % target)
+        blog.build_blog('content', 'htdocs')
 
+@task
+def clean():
+    """Clean up build files."""
+    with lcd(paths.local_workspace()):
+        local('rm -rf build/*')
